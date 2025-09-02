523e7f0fe86eb6839db974c6b6c7b622fcde32b8
denisnagani <denisnagani1331@gmail.com>
2025-09-02 17:58:08 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 /////////////////=============////////////////////
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-
 void main() => runApp(MaterialApp(home: MyList()));
 
 class MyList extends StatefulWidget {
