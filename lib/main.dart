81688f001c3d3577f2fed1d1db51d088915c84e2
denisbhai <96648437+denisbhai@users.noreply.github.com>
2025-09-10 00:31:32 +0530

    Revert "merge code"

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,13 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-///==================================///
-// void main() {
-//   runApp(MyApp());
-// }
+///==============================///
+
 void main() {
   runApp(MyApp());
 }
+
+///==============================///
 
 class MyApp extends StatelessWidget {
   @override
